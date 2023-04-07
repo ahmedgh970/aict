513e4a7a0105f7c740cdeb50dbdb3c206eb4cbb0
Ahmed Ghorbel <74784846+ahmedgh970@users.noreply.github.com>
2023-04-07 13:48:37 +0200

    Update ConvNeXt.py

--- layers/ConvNeXt.py
+++ layers/ConvNeXt.py
@@ -1,7 +1,7 @@
 # ==============================================================================
 # MIT License
 
-# Copyright (c) 2022 Ahmed Ghorbel
+# Copyright (c) 2023 Ahmed Ghorbel
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
